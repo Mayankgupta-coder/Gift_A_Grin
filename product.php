d7404f97a75c82ce5ca3de0d6a167b72b63fd6bd
Divya Maheshwari <60094383+divya814@users.noreply.github.com>
2020-11-08 23:45:44 +0530

    Update product.php

--- product.php
+++ product.php
@@ -20,27 +20,6 @@ if(isset($_GET['id'])){
 }
 ?>
 
- <!-- Start Bradcaump area -->
-        <div class="ht__bradcaump__area" style="background: rgba(0, 0, 0, 0) url(images/bg/4.jpg) no-repeat scroll center center / cover ;">
-            <div class="ht__bradcaump__wrap">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-xs-12">
-                            <div class="bradcaump__inner">
-                                <nav class="bradcaump-inner">
-                                  <a class="breadcrumb-item" href="index.php">Home</a>
-                                  <span class="brd-separetor"><i class="zmdi zmdi-chevron-right"></i></span>
-                                  <a class="breadcrumb-item" href="categories.php?id=<?php echo $get_product['0']['categories_id']?>"><?php echo $get_product['0']['categories']?></a>
-                                  <span class="brd-separetor"><i class="zmdi zmdi-chevron-right"></i></span>
-                                  <span class="breadcrumb-item active"><?php echo $get_product['0']['name']?></span>
-                                </nav>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <!-- End Bradcaump area -->
         <!-- Start Product Details Area -->
         <section class="htc__product__details bg__white ptb--100">
             <!-- Start Product Details Top -->
@@ -81,7 +60,7 @@ if(isset($_GET['id'])){
                                         </ul>
                                     </div>
                                     
-                                    </div>
+                                   </div>
 									
                                 </div>
                                 
