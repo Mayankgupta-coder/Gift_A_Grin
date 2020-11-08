06c857109d0c79cdef22d9d0d043f4f68776a622
Mayank Gupta <mynkgpt16@gmail.com>
2020-11-08 17:45:01 +0530

    add js in footer

--- footer.php
+++ footer.php
@@ -1,7 +1,23 @@
+<html>
 <footer class="footer text-center bg-dark">
     <div class="container">
       
       <p class="text-light">Copyright &copy; 2020 Gift A Grin</p>
       <p class="float-right">Images from Pixels and Pixabay</p>
     </div>
+    
   </footer>
+   <!-- jquery latest version -->
+   <script src="js/vendor/jquery-3.2.1.min.js"></script>
+    <!-- Bootstrap framework js -->
+    <script src="js/bootstrap.min.js"></script>
+    <!-- All js plugins included in this file. -->
+    <script src="js/plugins.js"></script>
+    <script src="js/slick.min.js"></script>
+    <script src="js/owl.carousel.min.js"></script>
+    <!-- Waypoints.min.js. -->
+    <script src="js/waypoints.min.js"></script>
+    <!-- Main js file that contents all jQuery plugins activation. -->
+    <script src="js/main.js"></script>
+    <script src="js/custom.js"></script>
+  <html>
