e20f32bc7715e550124c9608551db912581907a6
Divya Maheshwari <60094383+divya814@users.noreply.github.com>
2020-11-09 00:26:57 +0530

    Update index.php

--- index.php
+++ index.php
@@ -128,15 +128,15 @@
                                 <div class="category">
                                     <div class="ht__cat__thumb">
                                         <a href="product.php?id=<?php echo $list['id']?>">
-                                            <img src="<?php echo PRODUCT_IMAGE_SITE_PATH.$list['image']?>" alt="product images">
+                                            <img src="<?php echo PRODUCT_IMAGE_SITE_PATH.$list['image']?>" alt="product images" height="260px" width="120px">
                                         </a>
                                     </div>
-                                    <div class="fr__hover__info">
+                                    <!--<div class="fr__hover__info">
 										<ul class="product__action">
 											<li><a href="javascript:void(0)" onclick="wishlist_manage('<?php echo $list['id']?>','add')"><i class="icon-heart icons"></i></a></li>
 											<li><a href="javascript:void(0)" onclick="manage_cart('<?php echo $list['id']?>','add')"><i class="icon-handbag icons"></i></a></li>
 										</ul>
-									</div>
+									</div>-->
                                     <div class="fr__product__inner">
                                         <h4><a href="product.php?id=<?php echo $list['id']?>"><?php echo $list['name']?></a></h4>
                                         <ul class="fr__pro__prize">
@@ -175,15 +175,15 @@
                                 <div class="category">
                                     <div class="ht__cat__thumb">
                                         <a href="product.php?id=<?php echo $list['id']?>">
-                                            <img src="<?php echo PRODUCT_IMAGE_SITE_PATH.$list['image']?>" alt="product images">
+                                            <img src="<?php echo PRODUCT_IMAGE_SITE_PATH.$list['image']?>" alt="product images" height="260px" width="120px">
                                         </a>
                                     </div>
-                                    <div class="fr__hover__info">
+                                    <!--<div class="fr__hover__info">
 										<ul class="product__action">
 											<li><a href="javascript:void(0)" onclick="wishlist_manage('<?php echo $list['id']?>','add')"><i class="icon-heart icons"></i></a></li>
 											<li><a href="javascript:void(0)" onclick="manage_cart('<?php echo $list['id']?>','add')"><i class="icon-handbag icons"></i></a></li>
 										</ul>
-									</div>
+									</div>-->
                                     <div class="fr__product__inner">
                                         <h4><a href="product.php?id=<?php echo '₹'.$list['id']?>"><?php echo $list['name']?></a></h4>
                                         <ul class="fr__pro__prize">
