c84ec8760df0c6f5d98aadc6cfe4d3abe40e1c3d
Divya Maheshwari <60094383+divya814@users.noreply.github.com>
2020-11-08 23:40:53 +0530

    Added Slider

--- index.php
+++ index.php
@@ -3,6 +3,109 @@
         
        
         <!-- Start Slider Area -->
+<div class="slider__container slider--one bg__cat--3" style="background-color: rgb(67, 224, 247); padding:0">
+            <div class="slide__container slider__activation__wrap owl-carousel">
+                <!-- Start Single Slide -->
+                <div class="single__slide animation__style01 slider__fixed--height">
+                    <div class="container">
+                        <div class="row align-items__center">
+                            <div class="col-md-7 col-sm-7 col-xs-12 col-lg-6">
+                                <div class="slide">
+                                    <div class="slider__inner">                                        
+                                        <h2 style="line-height:34px">Book Diwali Presents for your loved ones</h2>
+                                        <h1>Amazing Stuff</h1>
+                                        <!-- <div class="cr__btn">
+                                            <a href="cart.html">Shop Now</a>
+                                        </div> -->
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-lg-6 col-sm-5 col-xs-12 col-md-5">
+                                <div class="slide__thumb">
+                                    <img src="img/c1.jpg" alt="slider images">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- End Single Slide -->
+                <!-- Start Single Slide -->
+                <div class="single__slide animation__style01 slider__fixed--height">
+                    <div class="container">
+                        <div class="row align-items__center">
+                            <div class="col-md-7 col-sm-7 col-xs-12 col-lg-6">
+                                <div class="slide">
+                                    <div class="slider__inner">
+                                    <h2 style="line-height:34px">Book Diwali Presents for your loved ones</h2>
+                                        <h1>Latest Cards Collection</h1>
+                                        
+                                        <!-- <div class="cr__btn">
+                                            <a href="cart.html">Shop Now</a>
+                                        </div> -->
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-lg-6 col-sm-5 col-xs-12 col-md-5">
+                                <div class="slide__thumb">
+                                    <img src="img/bday.jpg" alt="slider images">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- End Single Slide -->
+                <!-- Start Single Slide -->
+                <div class="single__slide animation__style01 slider__fixed--height">
+                    <div class="container">
+                        <div class="row align-items__center">
+                            <div class="col-md-7 col-sm-7 col-xs-12 col-lg-6">
+                                <div class="slide">
+                                    <div class="slider__inner">
+                                    <h2 style="line-height:34px">Book Diwali Presents for your loved ones</h2>
+                                       <h1>Customised Candles</h1>
+                                        <!-- <div class="cr__btn">
+                                            <a href="cart.html">Shop Now</a>
+                                        </div> --> -->
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-lg-6 col-sm-5 col-xs-12 col-md-5">
+                                <div class="slide__thumb">
+                                    <img src="img/c4.jpg" alt="slider images">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- End Single Slide -->
+                <!-- Start Single Slide -->
+                <div class="single__slide animation__style01 slider__fixed--height">
+                    <div class="container">
+                        <div class="row align-items__center">
+                            <div class="col-md-7 col-sm-7 col-xs-12 col-lg-6">
+                                <div class="slide">
+                                    <div class="slider__inner">
+                                    <h2 style="line-height:34px">Book Diwali Presents for your loved ones</h2>
+                                        <h1>Beautiful Lamps</h1>
+                                        <!-- <div class="cr__btn">
+                                            <a href="cart.html">Shop Now</a>
+                                        </div> -->
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-lg-6 col-sm-5 col-xs-12 col-md-5">
+                                <div class="slide__thumb">
+                                    <img src="img/lamp.jpg" alt="slider images">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- End Single Slide -->
+
+            </div>
+        </div>
+
         <!-- Start Category Area -->
         <section class="htc__category__area ptb--100">
             <div class="container">
