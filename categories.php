98b057f59127fb05f5cec040a2d003c89c725912
Divya Maheshwari <60094383+divya814@users.noreply.github.com>
2020-11-09 00:28:54 +0530

    Update categories.php

--- categories.php
+++ categories.php
@@ -21,43 +21,14 @@ if(isset($_GET['id']) && $_GET['id']!=''){
 ?>
 <div class="body__overlay"></div>
         
-        <!-- Start Bradcaump area -->
-        <div class="ht__bradcaump__area" style="background: rgba(0, 0, 0, 0) url(images/bg/4.jpg) no-repeat scroll center center / cover ;">
-            <div class="ht__bradcaump__wrap">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-xs-12">
-                            <div class="bradcaump__inner">
-                                <nav class="bradcaump-inner">
-                                  <a class="breadcrumb-item" href="index.php">Home</a>
-                                  <span class="brd-separetor"><i class="zmdi zmdi-chevron-right"></i></span>
-                                  <span class="breadcrumb-item active">Products</span>
-                                </nav>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <!-- End Bradcaump area -->
-        <!-- Start Product Grid -->
+        
         <section class="htc__product__grid bg__white ptb--100">
             <div class="container">
                 <div class="row">
 					<?php if(count($get_product)>0){?>
                     <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
                         <div class="htc__product__rightidebar">
-                            <!-- <div class="htc__grid__top">
-                                <div class="htc__select__option">
-                                    <select class="ht__select">
-                                        <option>Default softing</option>
-                                        <option>Sort by popularity</option>
-                                        <option>Sort by average rating</option>
-                                        <option>Sort by newness</option>
-                                    </select>
-                                </div>
-                               
-                            </div> -->
+                            
                             <!-- Start Product View -->
                             <div class="row">
                                 <div class="shop__grid__view__wrap">
@@ -70,7 +41,7 @@ if(isset($_GET['id']) && $_GET['id']!=''){
 											<div class="category">
 												<div class="ht__cat__thumb">
 													<a href="product.php?id=<?php echo $list['id']?>">
-														<img src="<?php echo PRODUCT_IMAGE_SITE_PATH.$list['image']?>" alt="product images">
+														<img src="<?php echo PRODUCT_IMAGE_SITE_PATH.$list['image']?>" alt="product images" height="260px" width="120px">
 													</a>
 												</div>
 												
